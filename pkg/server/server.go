7a337a803b93a40c4da0d595bd5a48601a3bca31
Eldad Izhaky <eldadizhaky@gmail.com>
2024-12-27 19:44:47 +0200

    feat(server): send back response

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net"
+
+	"github.com/Eldadcode/http-server/internal/http"
 )
 
 const (
@@ -19,13 +22,27 @@ func readEntirePacket(conn net.Conn) ([]byte, error) {
 }
 
 func handleConnection(conn net.Conn) {
+	var response_buffer []byte = []byte("HTTP/1.0 ")
 	defer conn.Close()
 	buf, err := readEntirePacket(conn)
 	if err != nil {
 		conn.Write([]byte("Error reading packet"))
 	}
-	fmt.Printf("Received: %s\n", string(buf))
+	http_response, err := http.HandleHTTPRequest(buf)
+	if err != nil {
+		conn.Write([]byte("Error with parsing HTTP request"))
+	}
 
+	response_buffer = append(response_buffer, fmt.Sprintf("%s\n", http_response.Status)...)
+	response_buffer = append(response_buffer, fmt.Sprintf("Server: %s\n", http_response.Server)...)
+	response_buffer = append(response_buffer, fmt.Sprintf("Date: %s\n", http_response.Date)...)
+	response_buffer = append(response_buffer, fmt.Sprintf("Content-type: %s\n", http_response.ContentType)...)
+	response_buffer = append(response_buffer, fmt.Sprintf("Content-Length: %d\n\n", http_response.ContentLength)...)
+	response_buffer = append(response_buffer, http_response.Content...)
+
+	fmt.Printf("%s\n", response_buffer)
+
+	conn.Write(response_buffer)
 }
 
 func StartServer(port uint16) error {
@@ -38,8 +55,8 @@ func StartServer(port uint16) error {
 		return err
 	}
 
-	fmt.Printf("Server started on http://localhost:%d\n", port)
-	
+	log.Printf("Server started on http://localhost:%d\n", port)
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
